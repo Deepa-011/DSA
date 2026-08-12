f954d656f3ca3115230d542b3302745b81dbc399
Deepa <gs2295483@gmail.com>
2026-08-12 21:58:25 +0530

    Create peak_element.cpp

---
+++ Searching/peak_element.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int low = 0, high = n - 1;
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] < arr[mid + 1])
+            low = mid + 1;
+        else
+            high = mid;
+    }
+
+    cout << "Peak element: " << arr[low];
+
+    return 0;
+}
