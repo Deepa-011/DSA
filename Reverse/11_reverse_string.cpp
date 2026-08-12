854c27eb8d5126474fd237945ce8cd52d47968b5
Deepa <gs2295483@gmail.com>
2026-08-12 19:13:46 +0530

    Create 11_reverse_string.cpp

---
+++ Reverse/11_reverse_string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+
+    int i = 0, j = str.length() - 1;
+
+    while (i < j) {
+        swap(str[i], str[j]);
+        i++;
+        j--;
+    }
+
+    cout << str;
+
+    return 0;
+}
