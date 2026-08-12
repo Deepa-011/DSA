3c921f919d0c03525d6443641a6729b4a45c54e0
Deepa <gs2295483@gmail.com>
2026-08-12 22:15:35 +0530

    Create count_vowels_constant.cpp

---
+++ String/count_vowels_constant.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+
+    int vowels = 0;
+    int consonants = 0;
+
+    for (char ch : str) {
+        ch = tolower(ch);
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    cout << "Vowels: " << vowels << endl;
+    cout << "Consonants: " << consonants;
+
+    return 0;
+}
